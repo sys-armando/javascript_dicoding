19ffc4dcd391c5b8747ee707d45462c573e7297f
sys-armando <manusiwapatrick@gmail.com>
2022-06-16 16:34:22 +0900

    update dulu gan

--- 1. fundamental_javascript/4. tipedata.js
+++ 1. fundamental_javascript/4. tipedata.js
@@ -61,4 +61,16 @@ console.log(c ** d)
  * Pada operator aritmatika juga terdapat operator increment (++) dan decrement (--). 
  * Operator increment dan decrement digunakan untuk menambahkan atau mengurangi nilai 1 pada nilai variabel 
  * yang ada sekarang.
+ * Operator ini dapat dituliskan sebelum atau sesudah variabel, tetapi hal tersebut bukan berarti sama. 
+ * Berikut ketentuannya:
+ * Jika dituliskan setelah variabel (x++), expression akan menghasilkan nilai variabel sebelum ditingkatkan nilainya.
+ * Jika dituliskan sebelum variabel (++x), expression akan menghasilkan nilai variabel setelah ditingkatkan nilainya.
+ * Untuk lebih jelasnya, berikut adalah contoh kode penerapan operator tersebut, perhatikan hasil yang didapat.
  */
+
+let postfix = 5;
+console.log(postfix++);
+console.log(postfix);
+
+let prefix = 5
+console.log(++prefix)
